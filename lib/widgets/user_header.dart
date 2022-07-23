1aeaf89e22db1c9bf9fc1f012b2999bc9169791e
Chzn <chzn@mail.sdu.edu.cn>
2022-07-23 23:30:31 +0800

    feat: 添加修改用户名功能
    添加了不完整的（缺发送到后端的那部分功能）修改用户名功能

--- lib/widgets/user_header.dart
+++ lib/widgets/user_header.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
+/*
+  这个注释与下个注释之间的内容
+  等待后端上线后修改
+*/
+Future<bool> _changeUserName(String newUsername) async {
+  // int uid=想个办法弄到uid;
+  // var response=await NetworkUtil.postRequest("user/name", {"uid":uid,"username":newUsername},null);
+  return true;
+}
+
+/* 结束 */
+enum _UsernameEditState { onEditing, uploading, finish }
+
 class UserHeader extends StatelessWidget {
   const UserHeader({Key? key, this.name, this.img}) : super(key: key);
 
@@ -13,17 +26,150 @@ class UserHeader extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 40.0),
       child: Row(
         children: [
-          const Icon(Icons.edit),
-          Text(name ?? 'DefaultUser'),
-          const Expanded(child: SizedBox()),
-          Image.network(
+
+          //这个LayoutBuilder用来让Text和Icon紧贴，并且Text不会因为超长溢出屏幕(所以没用Expanded(flex:0))
+          Expanded(child: _UserName(name)),
+
+          // const Expanded(child: SizedBox()),
+          ClipOval(
+              child: Image.network(
             img ?? 'https://www.sdu.edu.cn/images/logo.png',
             width: 90.0,
             height: 90.0,
             fit: BoxFit.cover,
-          )
+          ))
         ],
       ),
     );
   }
 }
+
+class _UserName extends StatefulWidget {
+  final String? name;
+
+  const _UserName(this.name);
+
+  @override
+  State<StatefulWidget> createState() => _UserNameState();
+}
+
+class _UserNameState extends State<_UserName> {
+  _UsernameEditState _state = _UsernameEditState.finish;
+  late String name, newName;
+  final TextEditingController controller = TextEditingController();
+
+  void _finishEdit() async {
+    if (newName == "" || newName == name) {
+      setState(() {
+        _state = _UsernameEditState.finish;
+        if (newName == "") controller.text = name;
+        newName = name;
+      });
+    }
+    String oldName = name;
+    setState(() {
+      name = newName;
+      _state = _UsernameEditState.uploading;
+    });
+    bool success = await _changeUserName(newName);
+    setState(() {
+      _state = _UsernameEditState.finish;
+      if (!success) {
+        name = oldName;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    name = widget.name ?? "DefaultUser";
+    newName = name;
+    controller.text = name;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double iconSize = IconTheme.of(context).size ?? 24;
+    final Image editIcon = Image(
+      image: const AssetImage("icons/编辑.png"),
+      height: iconSize,
+      width: iconSize,
+    );
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        return Row(
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                  maxWidth: constraint.maxWidth -
+                      iconSize -
+                      24 /*源自IconButton的padding*/),
+              child: _state != _UsernameEditState.onEditing
+                  ? Text(
+                      name,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textScaleFactor: 1.75,
+                    )
+                  : TextField(
+                      autofocus: true,
+                      controller: controller,
+                      decoration: const InputDecoration(
+                        isDense: true,
+                      ),
+                      cursorColor: Colors.white,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      onChanged: (val) {
+                        setState(() {
+                          newName = val;
+                        });
+                      },
+                      onEditingComplete: () {
+                        _finishEdit();
+                      },
+                    ),
+            ),
+            _state == _UsernameEditState.uploading
+                ? SizedBox(
+                    width: iconSize,
+                    height: iconSize,
+                    child: const CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+                  )
+                : _state == _UsernameEditState.onEditing
+                    ? IconButton(
+                        onPressed: () {
+                          _finishEdit();
+                        },
+                        icon: newName == "" || newName == name
+                            ? const Icon(
+                                Icons.clear,
+                                color: Colors.white,
+                              )
+                            : const Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              ))
+                    : IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _state = _UsernameEditState.onEditing;
+                          });
+                        },
+                        icon: editIcon),
+          ],
+        );
+      },
+    );
+  }
+}
+/*额外可以补充的功能*/
+// 点击空白处取消TextField焦点 https://www.jianshu.com/p/cc444d992ae7
+// 搜狗键盘收起但是TextField没有失去焦点解决方案 https://www.cnblogs.com/xsios/p/15821993.html
