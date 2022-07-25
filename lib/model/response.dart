61d03296cb688c51ab43f3fe8261db6a7403b372
guosurui <13012615613@163.com>
2022-07-25 17:39:38 +0800

    fix: 修改返回包装结构
    修改返回体构

--- lib/model/response.dart
+++ lib/model/response.dart
@@ -1,16 +1,18 @@
 
 /// response包装类
-class Response {
+class Response<T> {
   int? code;
   String? msg;
-  Object? data;
+  T? data;
+  List<T>? datas;
 
-  Response({this.code, this.msg, this.data});
+  Response({this.code, this.msg, this.data, this.datas});
 
   Response.fromJson(Map<String, dynamic> json) {
     code = json['code'];
     msg = json['msg'];
     data = json['data'];
+    datas = json['datas'];
   }
 
   Map<String, dynamic> toJson() {
@@ -18,6 +20,7 @@ class Response {
     data['code'] = code;
     data['msg'] = msg;
     data['data'] = this.data;
+    data['datas'] = datas;
     return data;
   }
 }
