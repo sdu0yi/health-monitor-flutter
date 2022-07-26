636f88d8d83aea954bad8a33467abaaef0193ce0
guosurui <13012615613@163.com>
2022-07-26 22:17:06 +0800

    feat: 添加食材获取后端数据的逻辑
    添加网络请求获取食材列表的后端数据并实现分页动态刷新

--- lib/routes/ingredient_menu.dart
+++ lib/routes/ingredient_menu.dart
@@ -1,5 +1,4 @@
-
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:heal_monitor_flutter/model/ingredient.dart';
 import 'package:heal_monitor_flutter/util/network_util.dart';
 import 'package:heal_monitor_flutter/widgets/ingredient_item.dart';
@@ -15,10 +14,27 @@ class IngredientMenu extends StatefulWidget {
 }
 
 class _IngredientMenuState extends State<IngredientMenu> {
+  static const int _pageSize = 20;
+  int _page = 0;
+  bool _more = true;
+  String? _query;
+  var _ingredientList = <Ingredient>[];
+  final ScrollController _controller = ScrollController();
+
   @override
   void initState() {
-    refreshData();
     super.initState();
+    _requestData();
+    _controller.addListener(() {
+      Logger().d(_controller.position.pixels);
+      Logger().d(_controller.position.maxScrollExtent);
+      if (_controller.position.pixels - _controller.position.maxScrollExtent <
+          20) {
+        if (_more) {
+          _requestData();
+        }
+      }
+    });
   }
 
   @override
@@ -36,28 +52,70 @@ class _IngredientMenuState extends State<IngredientMenu> {
           ),
         ),
         Expanded(
-            child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-          child: ListView(
-            children: const [IngredientItem()],
+          child: Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+            child: _ingredientList.isEmpty
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : RefreshIndicator(
+                    onRefresh: _refreshData,
+                    child: ListView.builder(
+                        controller: _controller,
+                        itemCount: _ingredientList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          if (index >= _ingredientList.length) {
+                            return Column(
+                              children: [
+                                IngredientItem(
+                                  name: _ingredientList[index].iname,
+                                ),
+                                const Divider(),
+                                const CircularProgressIndicator()
+                              ],
+                            );
+                          } else {
+                            return IngredientItem(
+                              name: _ingredientList[index].iname,
+                            );
+                          }
+                        })),
           ),
-        ))
+        )
       ],
     );
   }
-}
 
-Future<void> refreshData() async {
-  var response = await NetworkUtil.instance.get('/ingredient/getlist',
-      queryParameters: {"page_size": 20, 'page': 1, 'query': ''});
-  // var responseJson = jsonDecode(response.data);
-  Logger()
-      .d('CODE: ${response.statusCode}, MESSAGE: ${response.statusMessage}');
-  ResponseWrapper ds = ResponseWrapper.fromJson(response.data);
-  Logger().d(ds);
-  List<Ingredient>? list;
-  for (var element in ds.datas!) {
-    list?.add(Ingredient.fromJson(element));
-    Logger().d(Ingredient.fromJson(element));
+  Future<void> _refreshData() async {
+    _page = 0;
+    Logger().i('REFRESH DATA');
+  }
+
+  _requestData() async {
+    var response = await NetworkUtil.instance.get('/ingredient/getlist',
+        queryParameters: {
+          "page_size": _pageSize,
+          'page': _page,
+          'query': _query ?? ''
+        });
+    if (response.statusCode == 200) {
+      setState(() {
+        ResponseWrapper responseData = ResponseWrapper.fromJson(response.data);
+        List<Ingredient> responseList = [];
+        responseData.datas?.forEach((element) {
+          responseList.add(Ingredient.fromJson(element));
+          Logger().i(Ingredient.fromJson(element));
+        });
+
+        _page == 0
+            ? _ingredientList = responseList
+            : _ingredientList.addAll(responseList);
+        _page++;
+        if (responseList.length < _pageSize) {
+          _more = false;
+        }
+      });
+    }
   }
 }
